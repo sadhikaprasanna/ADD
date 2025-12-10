a309ff0cba46014ad70ecdd63767e91478e2f6f9
sadhikaprasanna <137609263+sadhikaprasanna@users.noreply.github.com>
2025-12-10 14:45:58 +0530

    commit

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -39,7 +39,7 @@ const Signup = () => {
 
     const { name, email, password, cpassword } = user;
 
-    const res = await fetch("/register", {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
