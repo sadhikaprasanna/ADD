b628b3b4a4f37f9f8d3f2a89c51c2f28627f3bc6
sadhikaprasanna <137609263+sadhikaprasanna@users.noreply.github.com>
2025-12-10 13:46:09 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -57,6 +57,8 @@ app.get('/old-prescriptions', async (req, res) => {
   
   
 
-app.listen(5000, () => {
-    console.log("Server is running on port no 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
